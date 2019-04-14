f94d5547249c9af3f9d684cc3e1a918de68d32ff
Marco Flores <floresm2199@outlook.com>
2019-04-13 22:38:46 -0500

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.311fc1d5d1d696b058e530800de00245.js"
+  "/precache-manifest.2de407c662a868fa4297ce01a7911769.js"
 );
 
 workbox.clientsClaim();
