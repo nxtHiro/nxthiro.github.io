0dd3c8c265eb966b6dab53f8f531682925a83755
Marco Flores <floresm2199@outlook.com>
2019-04-14 15:11:17 -0500

    Updates

---
+++ precache-manifest.ca15970240bc178a9b8c299dcb93d5f7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c075818cf7f09d0e9d10bf180e425c4",
+    "url": "/static/media/twitter.2c075818.png"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c9bcd055da5894ee6002",
+    "url": "/static/js/main.a6f3e468.chunk.js"
+  },
+  {
+    "revision": "72acccea1fb5d4036530",
+    "url": "/static/js/2.553c6935.chunk.js"
+  },
+  {
+    "revision": "c9bcd055da5894ee6002",
+    "url": "/static/css/main.ebb09a0c.chunk.css"
+  },
+  {
+    "revision": "b207440f3abeae9368d310eb8253fd8e",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6f82795653bc1e6fb07be5eccecf9aa.js"
+  "/precache-manifest.ca15970240bc178a9b8c299dcb93d5f7.js"
 );
 
 workbox.clientsClaim();
