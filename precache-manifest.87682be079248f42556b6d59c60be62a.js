c58a076f83c0cb505db2f0a6330f489925a46825
Marco Flores <floresm2199@outlook.com>
2019-04-13 21:42:59 -0500

    Updates

---
+++ precache-manifest.87682be079248f42556b6d59c60be62a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c4f5ed5275f13cf0414d",
+    "url": "/static/js/main.f7d9a729.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c4f5ed5275f13cf0414d",
+    "url": "/static/css/main.4bf51bf4.chunk.css"
+  },
+  {
+    "revision": "44d6b01cf8c2e5bd70c6f796f77d3e94",
+    "url": "/index.html"
+  }
+];
